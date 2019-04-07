871629e908f21771d4603787e778d7b5efcb0f44
Frank <fr.heggeness@gmail.com>
2019-04-06 18:30:49 -1000

    4/4

--- insertionSort.js
+++ insertionSort.js
@@ -2,14 +2,14 @@ let dummyArray = [5, 14, 23, 50, 2, 1, 0]
 
 function insertionSort(array) {
   for (var i = 0; i < array.length; i++) {
-    var place = array[i];
+    var placeOnArray = array[i];
     var j = i - 1;
-    while (j >= 0 && array[j] > place) {
+    while (j >= 0 && array[j] > placeOnArray) {
       array[j + 1] = array[j];
       j--;
     }
     //     console.log(j)
-    array[j + 1] = place;
+    array[j + 1] = placeOnArray;
     //     console.log(place)
     //     console.log(array[i])
   }
